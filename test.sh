675ce65d1075b84f94245329fd19cc91abb6d679
Byurrer <byurrer@mail.ru>
2022-09-01 16:33:37 +0300

    bugfix, changed user

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@
 [ -d logs ] || mkdir logs -m=777
 [ -d coverage-report-html ] || mkdir logs -m=777
 docker compose -f docker-compose.test.yml up -d --force-recreate --build
-docker compose exec -T backend /bin/bash -c 'composer install'
-docker compose exec -T backend /bin/bash -c 'composer run-script cs'
-docker compose exec -T backend /bin/bash -c 'composer run-script lint'
-docker compose exec -T backend /bin/bash -c 'composer run-script test-unit'
-docker compose exec -T backend /bin/bash -c 'composer run-script test-server'
-docker compose exec -T backend /bin/bash -c 'php -f wait-db.php'
-docker compose exec -T backend /bin/bash -c 'composer run-script test-system'
+docker compose -f docker-compose.test.yml exec -T backend /bin/bash -c 'composer install'
+docker compose -f docker-compose.test.yml exec -T backend /bin/bash -c 'composer run-script cs'
+docker compose -f docker-compose.test.yml exec -T backend /bin/bash -c 'composer run-script lint'
+docker compose -f docker-compose.test.yml exec -T backend /bin/bash -c 'composer run-script test-unit'
+docker compose -f docker-compose.test.yml exec -T backend /bin/bash -c 'composer run-script test-server'
+docker compose -f docker-compose.test.yml exec -T backend /bin/bash -c 'php -f wait-db.php'
+docker compose -f docker-compose.test.yml exec -T backend /bin/bash -c 'composer run-script test-system'
 docker compose down
