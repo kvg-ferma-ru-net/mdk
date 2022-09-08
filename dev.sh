c645f33671213807b30c9793c1763a624a2e42d7
Byurrer <byurrer@mail.ru>
2022-09-08 11:03:15 +0300

    fixed

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 [ -d logs ] || mkdir logs -m=777
-[ -d coverage-report-html ] || mkdir logs -m=777
+[ -d coverage-report-html ] || mkdir coverage-report-html -m=777
 docker compose -f docker-compose.dev.yml up --force-recreate --build
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 [ -d logs ] || mkdir logs -m=777
-[ -d coverage-report-html ] || mkdir logs -m=777
+[ -d coverage-report-html ] || mkdir coverage-report-html -m=777
 docker compose -f docker-compose.test.yml up -d --force-recreate --build
 docker compose -f docker-compose.test.yml exec -T backend /bin/bash -c 'composer install'
 docker compose -f docker-compose.test.yml exec -T backend /bin/bash -c 'composer run-script cs'
