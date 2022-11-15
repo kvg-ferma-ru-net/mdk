8fe2014a9229741475db07dddcfb60ddd717c45b
Byurrer <byurrer@mail.ru>
2022-11-15 14:53:47 +0300

    fixed

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 docker compose exec -T backend /bin/bash -c 'composer run cs' \
 && docker compose exec -T backend /bin/bash -c 'composer run lint' \
 && docker compose exec -T backend /bin/bash -c 'composer run test-unit' \
-&& docker compose -T backend /bin/bash -c 'composer run test-server' \
-&& docker compose -T backend /bin/bash -c 'php -f wait-db.php' \
+&& docker compose exec -T backend /bin/bash -c 'composer run test-server' \
+&& docker compose exec -T backend /bin/bash -c 'php -f wait-db.php' \
 && docker compose exec -T backend /bin/bash -c 'composer run test-system'
